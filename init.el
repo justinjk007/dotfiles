1dfe6cdb6941146ec4b1a8ebb7b718c35aee30af
Justin K -Linux <justinjoseph0007@gmail.com>
2017-04-08 18:28:50 -0400

    web-mode for css is a bad idea, revert

--- init.el
+++ init.el
@@ -299,7 +299,6 @@
 ;;-------------------------WEB-mode--------------
 (require 'web-mode)
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.css\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.php\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
