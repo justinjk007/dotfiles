b88b6cc8ec9dc483e1c07e0ac8c6e8def8897524
Justin K -Linux <justinjoseph0007@gmail.com>
2017-04-15 00:43:19 -0400

    Changed linum color

--- init.el
+++ init.el
@@ -220,7 +220,7 @@
  '(error ((t (:foreground "indian red" :weight bold))))
  '(highlight-numbers-number ((t (:inherit nil :foreground "coral1"))))
  '(lazy-highlight ((t (:background "gray17" :foreground "orange red" :weight bold))))
- '(linum ((t (:background "#fdf6e3" :distant-foreground "black" :foreground "#93a1a1" :box nil :strike-through nil :underline nil :weight thin :height 100 :width normal :family "Raleway Medium"))))
+ '(linum ((t (:background "#fdf6e3" :foreground "black" :distant-foreground "black" :foreground "#93a1a1" :box nil :strike-through nil :underline nil :weight thin :height 100 :width normal :family "Raleway Medium"))))
  '(minibuffer-prompt ((t (:inherit nil :box nil :background nil :foreground "red"))))
  '(org-default ((t (:family "Hack"))))
  '(org-level-1 ((t (:foreground "#7c91ea" :weight light :height 1.1))))
