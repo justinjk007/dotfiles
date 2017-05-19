e92ae2623b691f353c8899cf2c14cbe77a1f4770
Justin K -W10 <justinjoseph0007@gmail.com>
2017-05-19 13:36:13 -0400

    added binding for councel-yank-pop

--- init.el
+++ init.el
@@ -81,7 +81,6 @@
   )
 
 ;; (use-package company-ycmd
-;;   :ensure t
 ;;   :after ycmd
 ;;   :config
 ;;   (push '(company-ycmd :with company-yasnippet company-dabbrev-code) company-backends)
@@ -470,6 +469,7 @@
 (use-package counsel
   :config
   (global-set-key (kbd "M-x") 'counsel-M-x)
+  (global-set-key (kbd "M-y") 'counsel-yank-pop)
   (global-set-key (kbd "C-x C-f") 'counsel-find-file)
   (global-set-key (kbd "<f1> f") 'counsel-describe-function)
   (global-set-key (kbd "<f1> v") 'counsel-describe-variable)
