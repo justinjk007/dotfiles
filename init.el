2bf266d4bbcbd41db1da4d6fc94c47a602799bcb
Justin K -W10 <justinjoseph0007@gmail.com>
2017-05-19 14:01:10 -0400

    Fixed hundry delete diminish, silly me

--- init.el
+++ init.el
@@ -495,8 +495,8 @@
 
 (use-package hungry-delete
   ;; deletes all the whitespace when you hit backspace or delete
-  :config
   :diminish hungry-delete-mode
+  :config
   (global-hungry-delete-mode)
   )
 
