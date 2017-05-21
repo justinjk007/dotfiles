18f5cc98cf2b2af7b9b42d302e5f4aa9fc8e8098
Justin K -W10 <justinjoseph0007@gmail.com>
2017-05-21 17:50:34 -0400

    Moved custom-settings into a new file

--- init.el
+++ init.el
@@ -129,6 +129,8 @@
   )
 
 (load-file "~/.emacs.d/custom-functions.el") ;; Loads my custom-functions
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
 
 (setq visible-bell nil
       ring-bell-function 'my-terminal-visible-bell)
@@ -154,99 +156,6 @@
       kept-new-versions 3    ; keep some new versions
       kept-old-versions 2)   ; and some old ones, too
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(airline-cursor-colors nil)
- '(airline-display-directory nil)
- '(ansi-color-faces-vector
-   [default default default italic underline success warning error])
- '(ansi-color-names-vector
-   ["#2d3743" "#ff4242" "#74af68" "#dbdb95" "#34cae2" "#008b8b" "#00ede1" "#e1e1e0"])
- '(cursor-type (quote (bar . 3)))
- '(custom-enabled-themes (quote (solarized-light)))
- '(custom-safe-themes
-   (quote
-    ("2b8dff32b9018d88e24044eb60d8f3829bd6bbeab754e70799b78593af1c3aba"
-     "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879"
-     "4d5ce1a2a9c608a48f0489971bf2ebededdef436107fb4278278043e37062f79"
-     "6e771f5545f720302e62fedb0adf8b254f58c1916f54dbb2df11614fc9e24c67"
-     "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4"
-     "23cf1bbd82721df1785aa1a10f742e555d6ea41921b65fab0345947bdd56c3f8" default)))
- '(custom-theme-load-path
-   (quote
-    ("~/.emacs.d/elpa/airline-themes-20170425.1646"
-     "~/.emacs.d/elpa/solarized-theme-1.2.2" custom-theme-directory t)))
- '(electric-pair-mode t)
- '(flycheck-indication-mode (quote right-fringe))
- '(flyspell-abbrev-p t)
- '(frame-background-mode (quote dark))
- '(global-flycheck-mode t)
- '(global-hl-line-mode t)
- '(inhibit-startup-screen t)
- '(org-agenda-files(quote("D:/Dropbox/org-files/todo")))
- '(org-agenda-todo-ignore-deadlines nil)
- '(org-agenda-todo-ignore-schedules nil)
- '(org-hide-leading-stars t)
- '(org-startup-indented t)
- '(package-enable-at-startup t)
- '(package-selected-packages
-   (quote
-    (org
-     flyspell-popup
-     flyspell-correct
-     evil
-     flycheck
-     rainbow-mode
-     yasnippet
-     solarized-theme
-     emmet-mode)))
- '(powerline-height nil)
- '(python-shell-exec-path (quote ("C:/Python27")))
- '(python-shell-prompt-detect-failure-warning t)
- '(python-shell-virtualenv-root nil)
- '(ring-bell-function (quote ignore))
- '(ring-bell-function (quote ignore) t)
- '(scroll-restore-mode t)
- '(send-mail-function (quote mailclient-send-it))
- '(show-paren-mode t)
- '(solarized-distinct-doc-face nil)
- '(solarized-use-variable-pitch nil)
- '(standard-indent 2))
-
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 100 :width normal :foundry "outline" :family "Hack"))))
- '(column-marker-1 ((t (:background "dim grey"))))
- '(all-the-icons-dired-dir-face ((t (:foreground "#8b7d7d"))))
- '(airline-emacs-inner ((t (:foreground "orange red"))))
- '(airline-normal-inner ((t (:foreground "orange red"))))
- '(comint-highlight-prompt ((t (:foreground "orange red"))))
- '(cursor ((t (:background "#FF7D9E"))))
- '(fic-face ((t (:foreground "#ff6a6a" :slant normal :weight bold))))
- '(error ((t (:foreground "indian red" :weight bold))))
- '(highlight-numbers-number ((t (:inherit nil :foreground "coral1"))))
- '(ivy-minibuffer-match-face-2 ((t (:background "light steel blue" :weight bold))))
- '(lazy-highlight ((t (:background "gray17" :foreground "orange red" :weight bold))))
- '(font-lock-reference-face ((t (:foreground "firebrick1"))))
- '(linum ((t (:background "#fdf6e3" :foreground "black" :distant-foreground "black" :foreground "#93a1a1" :box nil :strike-through nil :underline nil :weight thin :height 100 :width normal :family "Raleway Medium"))))
- '(minibuffer-prompt ((t (:inherit nil :box nil :background nil :foreground "red"))))
- '(org-default ((t (:family "Hack"))))
- '(org-level-1 ((t (:foreground "#7c91ea" :family "Hack"))))
- '(org-level-2 ((t (:foreground "plum" :family "Hack"))))
- '(org-level-3 ((t (:foreground "pink" :family "Hack"))))
- '(org-link ((t (:foreground "SkyBlue1" :underline t))))
- '(org-verbatim ((t (:foreground "tomato"))))
- '(org-warning ((t (:foreground "red" :underline t))))
- '(powerline-inactive1 ((t (:background "dim gray" :foreground "white"))))
- '(powerline-inactive2 ((t (:background "#4a4a4a" :foreground "pink"))))
- '(web-mode-folded-face ((t (:foreground "#F6358A" :underline nil)))))
-(set-face-attribute 'region nil :foreground "#2aa198" :background "#586e75")
 
 ;; make electric-pair-mode work on more brackets
 (defvar electric-pair-pairs '(
