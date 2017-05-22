d5a127b1fc64becff158a14258c5b1f96457fbbd
Justin K -Linux <justinjoseph0007@gmail.com>
2017-05-22 11:20:05 -0400

    added .zshrc and snippets

--- setup.sh
+++ setup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-cp snippets ~/.emacs.d/
 cp custom-functions.el ~/.emacs.d/
 cp custom.el ~/.emacs.d/
 cp .gitconfig ~/
 cp .aliases ~/
+cp .zshrc ~/
 cp init.el ~/.emacs.d/
-echo "Finished copying 5 files and 1 folder"
+cp -rf snippets ~/.emacs.d/
+echo "Finished copying 6 files and 1 folder"
