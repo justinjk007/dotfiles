f77b137579b9f5f7423b2babb61a663d6a5bff2c
Justin K -W10 <justinjoseph0007@gmail.com>
2017-05-22 09:27:34 -0400

    Added setup.sh

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cp snippets ~/.emacs.d/
+cp custom-functions.el ~/.emacs.d/
+cp custom.el ~/.emacs.d/
+cp .gitconfig ~/
+cp .aliases ~/
+cp init.el ~/.emacs.d/
+echo "Finished copying 5 files and 1 folder"
