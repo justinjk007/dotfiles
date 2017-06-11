810aea494da4d7de47be6b92bd38d4175ccaabfd
Justin K -W10 <justinjoseph0007@gmail.com>
2017-06-11 10:13:42 -0400

    This is better

--- copy.bat
+++ copy.bat
@@ -1,2 +1,6 @@
 @echo off
 copy init.el %home%\.emacs.d\init.el
+copy .gitconfig %home%\.gitconfig
+copy custom.el %home%\.emacs.d\custom.el
+copy custom-functions.el %home%\.emacs.d\custom-functions.el
+xcopy snippets %home%\.emacs.d\snippets
