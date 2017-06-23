9a7a46f2ab12c73c695765514f4b80fb5869e54c
Justin K -W10 <justinjoseph0007@gmail.com>
2017-06-23 11:39:01 -0400

    Added move-borders.el

--- copy.bat
+++ copy.bat
@@ -4,3 +4,4 @@ copy .gitconfig %home%\.gitconfig
 copy custom.el %home%\.emacs.d\custom.el
 copy custom-functions.el %home%\.emacs.d\custom-functions.el
 xcopy snippets %home%\.emacs.d\snippets
+xcopy lisp %home%\.emacs.d\lisp
