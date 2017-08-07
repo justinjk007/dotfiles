85f0a804ee8f521b0eda51fadfed0ddbd1805180
Justin K -W10 <justinjoseph0007@gmail.com>
2017-08-07 10:18:57 -0400

    Silenced problems messages from copying dotfiles

--- copy.bat
+++ copy.bat
@@ -3,8 +3,8 @@ rem Xcopy <Source> [<Destination>] [/w]
 rem Find more here --> https://msdn.microsoft.com/en-us/library/cc771254.aspx
 Xcopy init.el %home%\.emacs.d\init.el /e /y /f /q
 Xcopy .gitconfig %home%\.gitconfig /e /y /f /q
-Xcopy .clang_complete %home%\.clang_complete /e /y /f /q
-xcopy .clang-format %home%\.clang-format /e /y /f /q
+echo f | Xcopy .clang_complete %home%\.clang_complete /e /y /f /q
+echo f | Xcopy .clang-format %home%\.clang-format /e /y /f /q
 Xcopy custom.el %home%\.emacs.d\custom.el /e /y /f /q
 Xcopy custom-functions.el %home%\.emacs.d\custom-functions.el /e /y /f /q
 Xcopy snippets %home%\.emacs.d\snippets /e /y /f /q
