2e38d1203f73773992404ad511e2fb6495985857
Justin K -W10 <justinjoseph0007@gmail.com>
2017-09-01 16:48:43 -0400

    Added todo note for later

--- init.el
+++ init.el
@@ -527,6 +527,7 @@
   (add-hook 'c++-mode-hook 'maybe-cmake-project-hook)
   :config
   (setq cmake-project-default-build-dir-name "build\/")
+  ;; TODO Checkout cmake-ide https://github.com/atilaneves/cmake-ide
   )
 
 (use-package irony
