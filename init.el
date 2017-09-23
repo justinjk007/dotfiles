3664c77b77bd7f637660f75c3822196a0fef9b17
Justin K -W10 <justinjoseph0007@gmail.com>
2017-09-22 22:14:48 -0400

    Better owner info

--- init.el
+++ init.el
@@ -16,6 +16,9 @@
 (package-initialize)
 (setq gc-cons-threshold 20000000)
 (setq garbage-collection-messages t)
+(setq user-full-name "Justin Kaipada"
+      user-mail-address "justinjoseph0007@gmail.com"
+      )
 (setq initial-scratch-message nil)
 (setq message-log-max 10000)
 (fset 'yes-or-no-p 'y-or-n-p); Change yes/no to y/n
