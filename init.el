e5c6db6ca72b4351296cc869b474927e99939f06
Justin K -W10 <justinjoseph0007@gmail.com>
2017-09-28 11:24:25 -0400

    gc-cons-threshold gets a new value

--- init.el
+++ init.el
@@ -14,7 +14,7 @@
                          ))
 
 (package-initialize)
-(setq gc-cons-threshold 20000000)
+(setq gc-cons-threshold (* 50 1024 1024))
 (setq user-full-name "Justin Kaipada"
       user-mail-address "justinjoseph0007@gmail.com"
       )
@@ -740,7 +740,6 @@ _k_: previous error    _l_: last error
 (global-set-key (kbd "C-c k o b") 'kill-other-buffers)
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
 
-(setq gc-cons-threshold 800000) ;; Back to default
 
 ;;  ) ;; !IMPORTANT for closing the file name handler, see begining of file
 (provide 'init.el)
