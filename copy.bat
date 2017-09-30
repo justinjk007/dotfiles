d922e7058d4e61c29cb1f961b940c2a564c17ab6
Justin K <justinjoseph0007@gmail.com>
2017-09-30 11:47:59 -0400

    I don't want this to be replaced everytime

--- copy.bat
+++ copy.bat
@@ -2,7 +2,6 @@
 rem Xcopy <Source> [<Destination>] [/w]
 rem Find more here --> https://msdn.microsoft.com/en-us/library/cc771254.aspx
 Xcopy init.el %home%\.emacs.d\init.el /e /y /f /q
-Xcopy .gitconfig %home%\.gitconfig /e /y /f /q
 echo f | Xcopy .clang_complete %home%\.clang_complete /e /y /f /q
 echo f | Xcopy .clang-format %home%\.clang-format /e /y /f /q
 Xcopy custom.el %home%\.emacs.d\custom.el /e /y /f /q
--- copy.sh
+++ copy.sh
@@ -2,7 +2,6 @@
 cp init.el ~/.emacs.d/
 cp custom-functions.el ~/.emacs.d/
 cp custom.el ~/.emacs.d/
-cp .gitconfig ~/
 cp .aliases ~/
 cp .zshrc ~/
 cp .clang_complete ~/
