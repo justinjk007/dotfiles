385d2d2ff8605a83fe6eb9213281b561f9f74aee
Justin K -W10 <justinjoseph0007@gmail.com>
2018-01-20 17:51:04 -0500

    Organized lisp files

--- copy.bat
+++ copy.bat
@@ -2,12 +2,10 @@
 rem Xcopy <Source> [<Destination>] [/w]
 rem Find more here --> https://msdn.microsoft.com/en-us/library/cc771254.aspx
 Xcopy init.el %home%\.emacs.d\init.el /e /y /f /q
-Xcopy myhydras.el %home%\.emacs.d\myhydras.el /e /y /f /q
+Xcopy lisp\myhydras.el %home%\.emacs.d\myhydras.el /e /y /f /q
+Xcopy lisp\custom.el %home%\.emacs.d\custom.el /e /y /f /q
+Xcopy lisp\custom-functions.el %home%\.emacs.d\custom-functions.el /e /y /f /q
+echo f | Xcopy .ycm_extra_conf.py %home%\.ycm_extra_conf.py /e /y /f /q
 echo f | Xcopy .clang_complete %home%\.clang_complete /e /y /f /q
 echo f | Xcopy .clang-format %home%\.clang-format /e /y /f /q
-Xcopy custom.el %home%\.emacs.d\custom.el /e /y /f /q
-Xcopy clang-windows-include-fix.h %home%\.emacs.d\clang-windows-include-fix.h /e /y /f /q
-Xcopy custom-functions.el %home%\.emacs.d\custom-functions.el /e /y /f /q
 Xcopy snippets %home%\.emacs.d\snippets /e /y /f /q
-Xcopy lisp %home%\.emacs.d\lisp /e /y /f /q
-echo f | Xcopy .ycm_extra_conf.py %home%\.ycm_extra_conf.py /e /y /f /q
--- copy.sh
+++ copy.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 cp init.el ~/.emacs.d/
-cp custom-functions.el ~/.emacs.d/
-cp custom.el ~/.emacs.d/
-cp myhydras.el ~/.emacs.d/
-cp clang-windows-include-fix.h ~/.emacs.d/
-cp .aliases ~/
-cp .zshrc ~/
+cp lisp/myhydras.el ~/.emacs.d/
+cp lisp/custom.el ~/.emacs.d/
+cp lisp/custom-functions.el ~/.emacs.d/
+cp .ycm_extra_conf.py ~/
 cp .clang_complete ~/
 cp .clang-format ~/
 cp -rf snippets ~/.emacs.d/
-cp .ycm_extra_conf.py ~/
+cp .aliases ~/
+cp .zshrc ~/
