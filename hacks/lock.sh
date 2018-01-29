c4b845eea47bba18e8c00a7a1777022313dfb8b1
Justin <justinjoseph0007@gmail.com>
2018-01-29 11:10:17 -0500

    Deleted lock.sh, normal lock screen seems better now

--- hacks/lock.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-scrot /tmp/screenshot.png
-convert /tmp/screenshot.png -blur 0x5 /tmp/screenshotblur.png
-i3lock -i /tmp/screenshotblur.png
-
-# Install by
-# sudo apt-get install i3lock scrot imagemagick xautolock
-# File should be /bin/lock
-# sudo chmod +x /bin/lock --> Make it into an executable
