8e7a39679455e89b16f14107bd83b901a8b28b73
Justin K -W10 <justinjoseph0007@gmail.com>
2018-04-15 09:40:25 -0400

    Javascript you punny little thing

--- init.el
+++ init.el
@@ -664,6 +664,29 @@
   :ensure auctex
   )
 
+(use-package js2-mode
+  :mode ("\\.js\\'" . js2-mode)
+  :init
+  (add-hook 'js2-mode-hook #'js2-imenu-extras-mode)
+  )
+
+(use-package js2-refactor
+  :after js2-mode
+  :init
+  (add-hook 'js2-mode-hook #'js2-refactor-mode)
+  :config
+  (js2r-add-keybindings-with-prefix "C-c C-r")
+  (define-key js2-mode-map (kbd "C-k") #'js2r-kill)
+  (define-key js-mode-map (kbd "M-.") nil) ;; Unbind
+  )
+
+(use-package xref-js2
+  :after js2-mode
+  :config
+  (add-hook 'js2-mode-hook (lambda ()
+			     (add-hook 'xref-backend-functions #'xref-js2-xref-backend nil t)))
+  )
+
 (use-package origami
   :defer t
   :init
