2714578df17c49645967438a54469aa2fa23adc5
Justin K -Linux <justinjoseph0007@gmail.com>
2018-06-04 22:48:39 -0400

    Removed more warnings, I here emacs 26 is getting stricter

--- lisp/myhydras.el
+++ lisp/myhydras.el
@@ -1,3 +1,9 @@
+;;; package --- myhydras
+;;; Author:Justin Kaipada
+;;; Commentary:
+;;; This is the file that contains all my hydras
+;;; Code:
+
 (global-set-key
  (kbd "C-c C-v")
  (defhydra hydra-toggle-stuff ()
@@ -60,11 +66,10 @@ _q_ quit            _i_ insert          _<_ previous
 
 (defun hot-expand (str &optional mod header)
   "Expand org template.
-
-STR is a structure template string recognised by org like <s. MOD is a
-string with additional parameters to add the begin line of the
-structure element. HEADER string includes more parameters that are
-prepended to the element after the #+HEADER: tag."
+  STR is a structure template string recognised by org like <s. MOD
+  is a string with additional parameters to add the begin line of
+  the structure element. HEADER string includes more parameters
+  that are prepended to the element after the #+HEADER: tag."
   (let (text)
     (when (region-active-p)
       (setq text (buffer-substring (region-beginning) (region-end)))
@@ -82,85 +87,9 @@ prepended to the element after the #+HEADER: tag."
       (if (or (region-active-p) (looking-back "^"))
 	  (hydra-org-template/body)
 	(self-insert-command 1)))))
-
 (eval-after-load "org"
   '(cl-pushnew
     '("not" "#+BEGIN_NOTES\n?\n#+END_NOTES")
     org-structure-template-alist))
 
-;; ------------------- Hydras for gnus --------------------------------.
-(eval-after-load 'gnus-group
-  '(progn
-     (defhydra hydra-gnus-group (:color blue)
-       "Do?"
-       ("a" gnus-group-list-active "REMOTE groups A A")
-       ("l" gnus-group-list-all-groups "LOCAL groups L")
-       ("c" gnus-topic-catchup-articles "Read all c")
-       ("G" gnus-group-make-nnir-group "Search server G G")
-       ("g" gnus-group-get-new-news "Refresh g")
-       ("s" gnus-group-enter-server-mode "Servers")
-       ("m" gnus-group-new-mail "Compose m OR C-x m")
-       ("#" gnus-topic-mark-topic "mark #")
-       ("q" nil "cancel"))
-     ;; y is not used by default
-     (define-key gnus-group-mode-map "y" 'hydra-gnus-group/body)))
-
-;; gnus-summary-mode
-(eval-after-load 'gnus-sum
-  '(progn
-     (defhydra hydra-gnus-summary (:color blue)
-       "Do ?:"
-       ("s" gnus-summary-show-thread "Show thread")
-       ("h" gnus-summary-hide-thread "Hide thread")
-       ("n" gnus-summary-insert-new-articles "Refresh / N")
-       ("f" gnus-summary-mail-forward "Forward C-c C-f")
-       ("!" gnus-summary-tick-article-forward "Mail -> disk !")
-       ("p" gnus-summary-put-mark-as-read "Mail <- disk")
-       ("c" gnus-summary-catchup-and-exit "Read all c")
-       ("e" gnus-summary-resend-message-edit "Resend S D e")
-       ("R" gnus-summary-reply-with-original "Reply with original R")
-       ("r" gnus-summary-reply "Reply r")
-       ("W" gnus-summary-wide-reply-with-original "Reply all with original S W")
-       ("w" gnus-summary-wide-reply "Reply all S w")
-       ("#" gnus-topic-mark-topic "mark #")
-       ("q" nil "cancel"))
-     ;; y is not used by default
-     (define-key gnus-summary-mode-map "y" 'hydra-gnus-summary/body)))
-
-;; gnus-article-mode
-(eval-after-load 'gnus-art
-  '(progn
-     (defhydra hydra-gnus-article (:color blue)
-       "Do?"
-       ("f" gnus-summary-mail-forward "Forward")
-       ("R" gnus-article-reply-with-original "Reply with original R")
-       ("r" gnus-article-reply "Reply r")
-       ("W" gnus-article-wide-reply-with-original "Reply all with original S W")
-       ("o" gnus-mime-save-part "Save attachment at point o")
-       ("w" gnus-article-wide-reply "Reply all S w")
-       ("q" nil "cancel"))
-     ;; y is not used by default
-     (define-key gnus-article-mode-map "y" 'hydra-gnus-article/body)))
-
-(defhydra hydra-undo-tree (:color yellow
-                                  :hint nil
-                                  )
-  "
-  _p_: undo  _n_: redo _s_: save _l_: load   "
-  ("p"   undo-tree-undo)
-  ("n"   undo-tree-redo)
-  ("s"   undo-tree-save-history)
-  ("l"   undo-tree-load-history)
-  ("u"   undo-tree-visualize "visualize" :color blue)
-  ("q"   nil "quit" :color blue))
-(global-set-key (kbd "M-,") 'hydra-undo-tree/undo-tree-undo) ;; or whatever
-
-(eval-after-load 'message
-  '(progn
-     (defhydra hydra-message (:color blue)
-       "Do?"
-       ("ca" mml-attach-file "Attach C-c C-a")
-       ("cc" message-send-and-exit "Send C-c C-c")
-       ("q" nil "cancel"))
-     (global-set-key (kbd "C-c C-y") 'hydra-message/body)))
-;; ------------------- Hydras for gnus --------------------------------.
+;;; myhydras.el ends here
