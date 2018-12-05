267079f89fe2507a4212163bdec8472cd82407bf
Justin K -Mac <justinjoseph0007@gmail.com>
2018-12-05 10:33:44 -0500

    Upgraded tp copy verbose ;)

--- copy.sh
+++ copy.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
-cp init.el ~/.emacs.d/
-cp lisp/myhydras.el ~/.emacs.d/
-cp lisp/custom.el ~/.emacs.d/
-cp lisp/custom-functions.el ~/.emacs.d/
-cp lisp/ox-mediawiki.el ~/.emacs.d/ox-mediawiki.el
-cp .ycm_extra_conf.py ~/
-cp .clang-format ~/
-cp -rf snippets ~/.emacs.d/
-cp linux/.aliases ~/
-cp linux/.bashrc ~/
-cp linux/.bash_profile ~/
+
+copy_verbose () {
+    echo "--- Copying     $1"
+    cp -rf $1 $2
+}
+
+copy_verbose init.el ~/.emacs.d/
+copy_verbose lisp/myhydras.el ~/.emacs.d/
+copy_verbose lisp/custom.el ~/.emacs.d/
+copy_verbose lisp/custom-functions.el ~/.emacs.d/
+copy_verbose lisp/ox-mediawiki.el ~/.emacs.d/ox-mediawiki.el
+copy_verbose .ycm_extra_conf.py ~/
+copy_verbose .clang-format ~/
+copy_verbose snippets ~/.emacs.d/
+copy_verbose linux/.aliases ~/
+copy_verbose linux/.bashrc ~/
+copy_verbose linux/.bash_profile ~/
