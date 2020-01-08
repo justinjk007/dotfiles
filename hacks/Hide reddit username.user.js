44d2ebe46eee9c1b3ec0697a7355c3be3ff007f6
Justin K -Linux <justinjoseph0007@gmail.com>
2020-01-08 14:05:25 -0500

    Hide me from peepers again

--- hacks/Hide reddit username.user.js
+++ hacks/Hide reddit username.user.js
@@ -5,7 +5,7 @@
 // @description  Hide my reddit username when logged in
 // @author       Me
 // @match        https://*.reddit.com/*
-// @grant        none
+// @grant        GM_addStyle
 // ==/UserScript==
 
 // This is a userscript for tampermonkey
@@ -13,4 +13,6 @@
 (function() {
     'use strict';
     document.querySelector('span._2BMnTatQ5gjKGK5OWROgaG').style.display='none'; // Hide username
+    document.querySelector('span._1pHyKCBktIf_9WFW9jjM3P').style.display='none'; // Hide my karma
+    document.querySelector('div._2kZkQ13N-kvhDEJOBd1S1I').style.display='none'; // Hide my username in the comment section
 })();
