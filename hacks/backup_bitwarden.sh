04dabf010937f9558554143be624a072a51df35e
Justin-WorkMac <justinjoseph0007@gmail.com>
2021-02-21 23:18:33 -0500

    Get master pass from env

--- hacks/backup_bitwarden.sh
+++ hacks/backup_bitwarden.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-MASTER_PASS=myPAssword123
-
 bw export $MASTER_PASS --format encrypted_json --output ./bitwarden_backup_encrypted.json
 cp bitwarden_backup_encrypted.json ~/Dropbox/
-sleep 10s
-rm bitwarden_backup_encrypted.json
+sleep 5s
+rm -rf bitwarden_backup_encrypted.json
