9bb4c53fdf8ffa0a147a1871345e784a03a5449f
Justin-WorkMac <justinjoseph0007@gmail.com>
2021-02-24 22:01:53 -0500

    Fix path, when running in crontab

--- hacks/backup_bitwarden.sh
+++ hacks/backup_bitwarden.sh
@@ -3,17 +3,18 @@
 # export these in .machine_config
 # export BW_SESSION=""
 # export MASTER_PASS=""
+source ~/.machine_config
 
-if /usr/local/bin/bw status | grep -q 'unlocked'; then
-    /usr/local/bin/bw export $MASTER_PASS --format encrypted_json --output ./bitwarden_backup_encrypted.json
+if bw status | grep -q 'unlocked'; then
+    bw export $MASTER_PASS --format encrypted_json --output ./bitwarden_backup_encrypted.json
     if [ -f bitwarden_backup_encrypted.json ]; then
 	cp bitwarden_backup_encrypted.json ~/Dropbox/
 	sleep 2s
 	rm -rf bitwarden_backup_encrypted.json
 	ls -l ~/Dropbox/bitwarden_backup_encrypted.json
     else
-	/usr/bin/osascript -e 'display notification "Bitwarden backup was NOT GENERATED"'
+	osascript -e 'display notification "Bitwarden backup was NOT GENERATED"'
     fi
 else
-    /usr/bin/osascript -e 'display notification "Bitwarden is LOCKED"'
+    osascript -e 'display notification "Bitwarden is LOCKED"'
 fi
